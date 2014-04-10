1a1c6e986db8b206535d67aa1f6b1920627e9eed
Kevin Gerami <kgerami@u.rochester.edu>
2014-04-09 21:27:31 -0400

    Created subdirectory and modified .gitignore to include DS_Store

--- blagtest/urls.py
+++ blagtest/urls.py
@@ -15,4 +15,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'core.views.list_strings'),
+    url(r'^test$', 'core.views.test'),
 )
--- core/views.py
+++ core/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def list_strings(request):
     all_strs = StringStore.objects.all()
     return render(request, 'list_strings.html', {'all_strs':all_strs, 'foo':'other content'})
+def test(request):
+    testObj = StringStore()
+    testObj = 'This is an object being displayed on a web page!'
+    return render(request,'test.html',{'test':testObj})
