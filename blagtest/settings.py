b22a7a14519133116108d5c6ade9f3f45a8af301
Kevin Gerami <kgerami@u.rochester.edu>
2014-04-10 00:10:35 -0400

    For some reason these two files did not commit with the others with the tutorial

--- blagtest/settings.py
+++ blagtest/settings.py
@@ -1,6 +1,6 @@
 # Django settings for blagtest project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -106,6 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/Users/kevingerami/Desktop/git/blagtest/mytemplates',
 )
 
 INSTALLED_APPS = (
@@ -116,10 +117,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'core',
+    'polls',
 )
 
 # A sample logging configuration. The only tangible logging
