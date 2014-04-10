848fbab17f61d1acd0cce0cd46bd2599d4120ffc
Alex Karpinski <akarpins@u.rochester.edu>
2014-04-09 20:06:56 -0400

    added basic model, template and view for core app

--- core/views.py
+++ core/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+from core.models import *
+from django.shortcuts import render
+
+def list_strings(request):
+    all_strs = StringStore.objects.all()
+    return render(request, 'list_strings.html', {'all_strs':all_strs, 'foo':'other content'})
