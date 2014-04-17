2690f18953aa2a1f770b0507a667b16dbd4bdadb
Kevin Gerami <kgerami@u.rochester.edu>
2014-04-16 21:07:34 -0400

    URL Calculator created

--- core/views.py
+++ core/views.py
@@ -10,4 +10,11 @@ def test(request):
     testObj = 'This is an object being displayed on a web page!'
     return render(request,'test.html',{'test':testObj})
 def add_nums(request, num1, num2):
-    pass
+    a=int(num1)+int(num2)
+    s=int(num1)-int(num2)
+    m=int(num1)*int(num2)
+    d=float(num1)/float(num2)
+    
+    return render(request, 'calc.html',{'add':a,'sub':s,'mult':m,'div':d,'one':num1,'two':num2})
+def mult_nums(request, num1, num2):
+    return render(request, 'calc.html',{'mult':int(num1)*int(num2)})
