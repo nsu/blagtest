14aad23fd0987ec4d66c4d629aab08cdf2511793
Alex Karpinski <akarpins@u.rochester.edu>
2014-04-23 19:16:30 -0400

    added blog basics

--- blagtest/urls.py
+++ blagtest/urls.py
@@ -14,6 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'core.views.list_strings'),
     url(r'^calc/(?P<num1>\d*)/(?P<num2>\d*)/$', 'core.views.add_nums'),
+    url(r'^blag/list/$', 'core.views.list_posts'),
 )
--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
 # Create your views here.
 from core.models import *
 from django.shortcuts import render
-
-def list_strings(request):
-    all_strs = StringStore.objects.all()
-    return render(request, 'list_strings.html', {'all_strs':all_strs, 'foo':'other content'})
+from datetime import datetime
 
 def add_nums(request, num1, num2):
     pass
+
+def list_posts(request):
+    all_posts = Post.objects.all()
+    return render(request, 'list_posts.html', {'posts': all_posts, 'today':datetime.now()})
