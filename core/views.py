08645cf64c9c821455d0d14990f880b19db21ee9
Scott Wittlinger <switti2@u.rochester.edu>
2014-04-23 20:34:53 -0400

    added user form to add posts

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@
 from core.models import *
 from django.shortcuts import render
 from datetime import datetime
+from django.http import HttpResponse, HttpResponseRedirect
+from forms import PostForm
 
 def add_nums(request, num1, num2):
     a = int(num1) + int(num2)
@@ -14,3 +16,13 @@ def mult_nums(request, num1, num2):
 def list_posts(request):
 	all_posts = Post.objects.all()
 	return render(request, 'list_posts.html', {'all_posts':all_posts, 'today':datetime.now()})
+
+def new_post(request):
+	if request.method == 'POST':
+		form = PostForm(request.POST)
+		if form.is_valid:
+			form.save()
+			return HttpResponseRedirect('/blag/list')
+	else:
+		form = PostForm()
+	return render(request, 'new_post.html', {'form':form})
